8009c53024dfae27aa956f0de9f4d01b1e883088
Yegor Bugayenko <yegor256@gmail.com>
2025-01-03 12:31:04 +0300

    year up to 2025

--- lib/others.rb
+++ lib/others.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2024 Yegor Bugayenko
+# Copyright (c) 2024-2025 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -23,7 +23,7 @@
 # A function that catches all undeclared methods.
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024 Yegor Bugayenko
+# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
 # License:: MIT
 def others(attrs = {}, &block)
   if is_a?(Class)
